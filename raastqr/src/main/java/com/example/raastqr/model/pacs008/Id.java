54e40a8a2332e77fd0e2bb94f181a6c6a0fc2d77
EshwarLal26 <eshwarlal@decryptedlabs.io>
2026-06-29 18:49:26 +0500

    id file

--- raastqr/src/main/java/com/example/raastqr/model/pacs008/Id.java
+++ raastqr/src/main/java/com/example/raastqr/model/pacs008/Id.java
@@ -1,14 +1,6 @@
 package com.example.raastqr.model.pacs008;
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
-@XmlAccessorType(XmlAccessType.FIELD)
-
 public class Id {
-    public void setIban(String iban) {
-        this.iban = iban;
-    }
-
     @XmlElement(name = "IBAN")
     private String iban;
 }
