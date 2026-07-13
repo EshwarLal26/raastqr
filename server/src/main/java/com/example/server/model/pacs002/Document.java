dac14b9eb2d73feb988fd0cd959cee72f62a2216
EshwarLal26 <eshwarlal@decryptedlabs.io>
2026-07-13 17:58:56 +0500

    add pacs002 model in server

---
+++ server/src/main/java/com/example/server/model/pacs002/Document.java
@@ -0,0 +1,20 @@
+package com.example.server.model.pacs002;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Document {
+
+    @XmlElement(name = "FIToFIPmtStsRpt", namespace = "urn:iso:std:iso:20022:tech:xsd:pacs.002.001.10")
+    private FIToFIPmtStsRpt fiToFiPmtStsRpt;
+
+    public FIToFIPmtStsRpt getFiToFiPmtStsRpt() {
+        return fiToFiPmtStsRpt;
+    }
+
+    public void setFiToFiPmtStsRpt(FIToFIPmtStsRpt fiToFiPmtStsRpt) {
+        this.fiToFiPmtStsRpt = fiToFiPmtStsRpt;
+    }
+}
